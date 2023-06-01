f83bf22fc4e60ce5e82f88b31a82141b25172466
alecanthony <alecanthony39@gmail.com>
2023-05-31 21:26:40 -0500

    sigh

--- db/activities.js
+++ db/activities.js
@@ -1,17 +1,53 @@
-const client = require('./client');
+const client = require("./client");
 
 // database functions
 async function createActivity({ name, description }) {
   // return the new activity
+
+  const { rows: activity } = await client.query(
+    `
+  INSERT INTO activities(name, description)
+  VALUES($1, $2)
+  RETURNING *;
+  `,
+    [name, description]
+  );
+
+  return activity[0];
 }
 
 async function getAllActivities() {
   // select and return an array of all activities
+  const { rows: activities } = await client.query(`
+  SELECT * FROM activities;`);
+
+  const active = await Promise.all(
+    activities.map((Act) => getActivityById(Act.id))
+  );
+
+  return active;
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  const { rows: activities } = await client.query(
+    ` SELECT *
+    FROM activities
+    WHERE id=$1`,
+    [id]
+  );
+  return activities[0];
+}
 
-async function getActivityByName(name) {}
+async function getActivityByName(name) {
+  const { rows: activities } = await client.query(
+    `
+  SELECT *
+  FROM activities
+  WHERE name=$1;`,
+    [name]
+  );
+  return activities[0];
+}
 
 // used as a helper inside db/routines.js
 async function attachActivitiesToRoutines(routines) {}
@@ -20,6 +56,21 @@ async function updateActivity({ id, ...fields }) {
   // don't try to update the id
   // do update the name and description
   // return the updated activity
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  if (setString.length > 0) {
+    const { rows: activities } = await client.query(
+      `
+UPDATE activities
+SET ${setString}
+WHERE id=$${Object.values(fields).length + 1}
+RETURNING *;`,
+      [...Object.values(fields), id]
+    );
+
+    return activities[0];
+  }
 }
 
 module.exports = {
--- db/users.js
+++ db/users.js
@@ -4,19 +4,68 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  const {
+    rows: [user],
+  } = await client.query(
+    `INSERT INTO users(username, password)
+      VALUES($1, $2)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *
+      `,
+    [username, password]
+  );
+  delete user.password;
+  return user;
 }
 
 async function getUser({ username, password }) {
+  const { rows: user } = await client.query(
+    `
+  SELECT * 
+  FROM users
+  WHERE username=$1`,
+    [username]
+  );
 
+  if (user[0].password === password) {
+    delete user[0].password;
+    return user[0];
+  }
 }
 
 async function getUserById(userId) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const { rows: user } = await client.query(
+      `SELECT * 
+    FROM users
+    WHERE id=${userId}`
+    );
+    if (!user) {
+      return null;
+    }
+    delete user[0].password;
 
+    return user[0];
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getUserByUsername(userName) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const { rows: user } = await client.query(
+      `SELECT *
+    FROM users
+    WHERE username=$1`,
+      [userName]
+    );
 
+    return user[0];
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
@@ -24,4 +73,4 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
